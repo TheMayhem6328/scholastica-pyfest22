1e55e090b25d16546e442f9041d4a82d2d7cf794
TheMayhem6328 <nebrus.gamer@gmail.com>
2022-09-26 09:20:30 +0600

    Commentate python file

--- Round01/Main.py
+++ Round01/Main.py
@@ -1,10 +1,10 @@
-import re
+import re # Regex support
 
 student_count = 5 # Assuming 5 for simplicity
 
 counter = 0
 while counter < student_count:
     temp = input("Enter student ID")
-    if re.search("\d{1,4}[-]{1}\d{1,2}[-]{1}\d{1,4}", temp):
+    if re.search("\d{1,4}[-]{1}\d{1,2}[-]{1}\d{1,4}", temp): # If input matches format `xxxx-xx-xxxx` (x being num)
         print("VALID")
     counter += 1
