36d9b1f0e72aee7eb1ab1dde3781b430b69a202a
TheMayhem6328 <nebrus.gamer@gmail.com>
2022-09-26 09:17:21 +0600

    Actually start solving the problem

--- Round01/Main.py
+++ Round01/Main.py
@@ -1,5 +1,10 @@
-def add(x,y):
-  z = x + y
-  return z
+import re
 
-print(add(1,2))
+student_count = 5 # Assuming 5 for simplicity
+
+counter = 0
+while counter < student_count:
+    temp = input("Enter student ID")
+    if re.search("\d{1,4}[-]{1}\d{1,2}[-]{1}\d{1,4}", temp):
+        print("VALID")
+    counter += 1
