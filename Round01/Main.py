ead9528446ca75a19317b4ed2d2ba50ac69c2355
TheMayhem6328 <nebrus.gamer@gmail.com>
2022-09-27 01:21:01 +0600

    Experimental changes

--- Round01/Main.py
+++ Round01/Main.py
@@ -1,7 +1,8 @@
 ###### Init and import stuff
-import re # Import regex library
+import re         # Import regex library
+import random     # Import random num gen library
 student_count = 5 # Assuming 5 for simplicity
-counter = 0 # Counter init
+counter = 0       # Counter init
 
 
 ###### Input, validation, collection and organisation start
@@ -35,7 +36,7 @@ while counter < student_count:
         # Don't append ID list if ID is in format `XXXX00XXXX` - but do increment counter
         if not re.search("\d{4}[0]{2}\d{4}",temp): # If ID does `not` match format `XXXX00XXXX`
 
-            # Duplicate check check
+            # Duplicate check
             flag2 = 0 # Will be more than 0 if duplicate found
 
             # Compare each element of ID list with ID input
@@ -82,4 +83,54 @@ id_list.sort()
 
 ###### Input, validation, collection and organisation end
 
+
+###### Password generation start
+
+### Declare a new list to store passwords in
+pwd_list = []
+
+### Re-initialise counter
+counter = 0
+
+### Generate password for each email
+# while True:
+# 
+#     # Init temp list
+#     temp_list = []
+# 
+#     # Convenience variables for functions
+#     tla = temp_list.append
+#     rnr = random.randint
+# 
+#     # Generate 3 numbers and dump them to list 
+#     tla(str(rnr(0,9)))
+#     tla(str(rnr(0,9)))
+# 
+#     # Generate 2 ASCII nums (for `UPPERCASE`` letter), convert them to characters and dump them to list
+#     tla(chr(rnr(65,90)))
+#     tla(chr(rnr(65,90)))
+#     tla(chr(rnr(65,90)))
+#     
+#     # Generate 3 ASCII nums (for `lowercase`` letter), convert them to characters and dump them to list
+#     tla(chr(rnr(97,122)))
+#     tla(chr(rnr(97,122)))
+#     tla(chr(rnr(97,122)))
+# 
+#     # Ensure characters don't follow a pattern
+#     random.shuffle(temp_list)
+# 
+#     # Declare a utility variable
+#     temp = ""
+# 
+#     # Build a string
+#     for x in temp_list:
+#         temp += temp_list
+# 
+#     # Append built string to password list
+#     pwd_list.append(temp)
+# 
+# print(pwd_list)
+
 print(id_list)
+
+###### Password generation end
