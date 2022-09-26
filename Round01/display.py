8f505bf1174b4ff2147e1464bdd4d071347aa1a6
Taz2040 <shababtasvirul1000@gmail.com>
2022-09-27 00:00:42 +0600

    Shabab display

--- Round01/display.py
+++ Round01/display.py
@@ -1,7 +1,9 @@
-idArr  = ["2013060031@scholastica.online","2015020001@scholastica.online"]
+idArr  = ["2013060031@scholastica.online","2015020001@scholastica.online","2008010188@scholastica.online"]
 pwdArr = ["vAdae233","adeE2342","asDs2323"]
 otpArr = ["153123","723483","236439"]
-
-print("Mail                          | Pwd  |  OTP")
+git
+print("         Mail                 |   Pwd    |  OTP  ")
+print("=================================================")
 for i in range(len(idArr)):
+    print(idArr[i],"|",pwdArr[i],"|",otpArr[i])
     
