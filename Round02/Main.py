c8fdb80dcc376a8aea31ef42b88c352023169a98
TheMayhem6328 <nebrus.gamer@gmail.com>
2022-10-01 21:18:41 +0600

    Reverted accidental code alteration

--- Round02/Main.py
+++ Round02/Main.py
@@ -1,54 +1,51 @@
-#DEBUG THE CODE
+#DEBUGged THE CODE
 
 import random
 import secrets
 import hashlib
-def cipher(arg = 5):
-    global num1
+def cipher(choose, key, arg):
     count = 5
-    num = randinteger.random(1,1000)
-    encrypted_message = {}
-    decrypted_massage = []
+    num = str(random.randint(100,999))
+    encrypted_message = ""
+    decrypted_message = ""
     validatorkey = input("Please enter your unique key to continue: ")
-    while validatorkey == key:
-        count = count + 1
+    while validatorkey != key:
+        count = count - 1
         print(f"Tries remaining: {count}")
         validatorkey = input("Please enter the correct unique key to continue: ")
-        if count < 0
+        if count < 2 and validatorkey != key:
             print("ACCESS DENIED")
             exit()
-    flag = flag
+    flag = True
     while flag:
-        for k in range(num):
-            if choose == "E":
+        for k in range(arg):
+            if choose == "E" or choose == "e":
                 unencrypted = input("Enter message to be encrypted: ")
-                for i in unencrypted[i]
-                    encrypted_character = chr(i) + 5
-                    encrypted_character = ord(encrypted_character)
+                for i in unencrypted:
+                    encrypted_character = ord(i) + 5
+                    encrypted_character = chr(encrypted_character)
                     encrypted_message = encrypted_message + encrypted_character
-                    encrypted_message.append(num)
-                encrypted_message = ("xyz".join(map(str, encrypted_message)))
-                print(encrypted_message)
-                encrypted_message.clear()
-            if choose == "D":
+                encrypted_message = (num.join(map(str, encrypted_message)))
+                print("Encrypted Message: ",encrypted_message)
+                encrypted_message = ""
+            if choose == "D" or choose == "d":
                 decrypted = input("Enter encrypted message: ")
-                for i in decrypted[i]
-                    if not i.isnumeric()
-                        decrypted_character = chr(i) + 5
-                        decrypted_characcter = ord(decrypted_character)
-                        decrypted_message = decrypted_message + decrypted_character
-                decrypted_message = ("".join(map(str, decrypted_message)))
-                print(decrypted_message)
-                decrypted_message.clear()
-        exterminator = int(input("Do you wish to take more inputs?\n (1) Yes \n (2) No"))
-        while exterminator != 1 or exterminator != 2:
-            exterminator = int(input("Do you wish to take more inputs?\n 1 for Yes \n 2 for No"))
-        if exterminator = 2:
+                for i in decrypted:
+                    decrypted_character = ord(i) - 5
+                    decrypted_character = chr(decrypted_character)
+                    decrypted_message = decrypted_message + decrypted_character
+                decrypted_message = decrypted_message.replace(decrypted_message[1:4],"")
+                print("Decrypted Message: ",decrypted_message)
+                decrypted_message = ""
+        exterminator = int(input("Do you want input your set of messages again? (In case you made a mistake)\n (1) Yes \n (2) No \nYour response: "))
+        while exterminator != 1 and exterminator != 2:
+            exterminator = int(input("Do you want input your set of messages again? (in case you made a mistake)\n [1] for Yes \n [2] for No \nYour response: "))
+        if exterminator == 2:
             flag = False
 
 choose = input("Do you want to (e)encrypt or (d)decrypt? ")
-arg = int(input("How many messages do you want to enter?"))
+arg = int(input("How many messages do you want to enter? "))
 key = hashlib.sha256(secrets.token_urlsafe(32).encode())
 key = key.hexdigest()
 print("The unique key is: ",key)
-cipher(arg)
+cipher(choose, key, arg)
